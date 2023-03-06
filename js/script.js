0e0c9942d966bce688c4eb396a2a4c82bd7ec2f6
ryanqle <ryan.q.le@gmail.com>
2023-03-06 16:40:43 -0600

    Working Event Listener, Computer Sequence, and Player turn

--- js/script.js
+++ js/script.js
@@ -6,7 +6,8 @@ const choices = ['y', 'r', 'g', 'b'];
 let cSequence = [];
 let pSequence = [];
 let round = 0;
-winner = false;
+let index = 0;
+let winner = false;
 
 /*----- cached element references -----*/
 const btnStart = document.getElementById('start');
@@ -15,6 +16,8 @@ const playArea = document.getElementById('play-area');
 const board = document.getElementById('board');
 const colors = document.querySelectorAll('.color');
 const arrOfColors = Array.from(colors);
+const roundDisplay = document.querySelector('h1');
+const infoDisplay = document.querySelector('h2');
 
 /*----- event listeners -----*/
 playArea.addEventListener('click', handleClick);
@@ -31,27 +34,82 @@ function handleClick(evt) {
         handlePlayerAction(playerAction.id);
     }
 }
-
-function init() {
-    cSequence = [];
-    pSequence = [];
-    round = 0;
-    nextRound();
-
-}
-
 function handleButton(btn) {
     if (btn === btnStart) {
         btnStart.style.visibility = 'hidden';
         init();
-
     }
     else if (btn === btnReset) {
         btnReset.style.visibility = 'hidden';
         init();
     }
 }
-
 function handlePlayerAction(colorClicked) {
-
+    pSequence.push(colorClicked)
+    let index = pSequence.length - 1;
+    if (pSequence[index] !== cSequence[index]) {
+        console.log('Game Over');
+        btnReset.style.visibility = 'visible';
+        board.classList.toggle('unclickable');
+        return;
+    }
+    if (pSequence.length === cSequence.length) {
+        pSequence = [];
+        board.classList.toggle('unclickable');
+        setTimeout(() => {
+            nextRound();
+        }, 500);
+    }
+    arrOfColors.forEach((color) => {
+        if (colorClicked === color.id) {
+            let colorIdx = arrOfColors.indexOf(color);
+            arrOfColors[colorIdx].style.borderColor = 'white';
+            indexOfLastColor = colorIdx;
+            setTimeout(() => {
+                arrOfColors[colorIdx].style.borderColor = 'black';
+            }, 200);
+        }
+    });
 }
+function init() {
+    cSequence = [];
+    pSequence = [];
+    round = 0;
+    index = 0;
+    nextRound();
+}
+function nextRound() {
+    round += 1;
+    roundDisplay.innerText = `Round ${round}`;
+    const nextSequence = choices[Math.floor(Math.random() * choices.length)];
+    cSequence.push(nextSequence);
+    playSequence(cSequence);
+}
+function playSequence(sequence) {
+    infoDisplay.innerText = `Wait for computer to play`;
+    while (index !== sequence.length) {
+        arrOfColors.forEach((color) => {
+            if (sequence[index] === color.id) {
+                let colorIdx = arrOfColors.indexOf(color);
+                setTimeout(() => {
+                    arrOfColors[colorIdx].style.borderColor = 'white';
+                }, (index + 1) * 1000);
+                setTimeout(() => {
+                    arrOfColors[colorIdx].style.borderColor = 'black';
+                }, (index + 1.5) * 1000);
+                clearTimeout();
+            }
+        });
+        index++;
+    }
+    index = 0;
+    playerTurn();
+}
+
+function playerTurn() {
+    setTimeout(() => {
+        board.classList.toggle('unclickable');
+        infoDisplay.innerText = `Your Turn`;
+    }, (round + 1) * 1000);
+}
+
